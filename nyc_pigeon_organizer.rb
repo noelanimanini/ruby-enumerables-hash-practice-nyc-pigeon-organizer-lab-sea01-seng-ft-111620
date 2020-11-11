76b63350d8aa24b0f5a22096185551381f76b16c
crystal  villanueva <noelani.villanueva@gmail.com>
2020-11-11 23:36:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,38 +1,38 @@
 def nyc_pigeon_organizer(data)
   pigeon_list ={
     "Theo" => {
-      :color => [:purple, :grey],
-      :gender => [:male],
+      :color => ["purple", "grey"],
+      :gender => ["male"],
       :lives => ["Subway"],
     },
     "Peter Jr." => {
-      :color => [:purple, :grey],
-      :gender => [:male],
+      :color => ["purple", "grey"],
+      :gender => ["male"],
       :lives => ["Library"],
     },
     "Lucky" => {
-      :color => [:purple],
-      :gender => [:male],
-      :lives => ["Central Park"] 
+      :color => ["purple"],
+      :gender => ["male"],
+      :lives => ["Central Park"]
     },
     "Ms. K" => {
-      :color => [:grey, :white],
-      :gender => [:female],
+      :color => ["grey", "white"],
+      :gender => ["female"],
       :lives => ["Central Park"],
     },
     "Queenie" => {
-      :color => [:white, :brown],
-      :gender => [:female],
+      :color => ["white", "brown"],
+      :gender => ["female"],
       :lives => ["Subway"],
     },
     "Andrew" => {
-      :color => [:white],
-      :gender => [:male],
+      :color => ["white"],
+      :gender => ["male"],
       :lives => ["City Hall"],
     },
     "Alex" => {
-      :color => [:white, :brown],
-      :gender => [:male],
+      :color => ["white", "brown"],
+      :gender => ["male"],
       :lives => ["Central Park"],
     }
 
