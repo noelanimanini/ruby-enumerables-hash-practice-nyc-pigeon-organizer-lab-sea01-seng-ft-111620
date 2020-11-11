8432121fcecc3b8a375c4030f96206e95a43d889
crystal  villanueva <noelani.villanueva@gmail.com>
2020-11-11 23:40:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -37,16 +37,4 @@ def nyc_pigeon_organizer(data)
     }
 
   }
-  pigeon_data = {
-    "Luca" => {
-      :color => ["brown"],
-      :gender => ["male"],
-      :lives => ["Library"],
-    },
-    "Lola" => {
-      :color => ["black"],
-      :gender => ["female"],
-      :lives => ["Central Park"],
-    }
-  }
 end
